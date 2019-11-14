c51de24cd2d1bdab9ab1ddec076868b28648ed34
Titus Tienaah <titus.intdxdt@gmail.com>
2019-11-14 23:51:15 +0000

    impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
-use rstar::{RTreeObject, RTree as Index, Envelope, PointDistance};
+use rstar::{
+    RTreeObject,
+    RTree as Index,
+    Envelope,
+    PointDistance,
+    AABB,
+};
+use bbox_2d::MBR;
 
 
 #[derive(Clone)]
@@ -21,9 +28,33 @@ impl<T> RTree<T> where T: RTreeObject + PointDistance {
         RTree { index: Index::bulk_load_with_params(item) }
     }
 
+
     pub fn insert(&mut self, item: T) {
         self.index.insert(item)
     }
+
+
+    pub fn size(&self) -> usize {
+        self.index.size()
+    }
+
+//    pub fn search(&self, envelope: &T::Envelope) -> Vec<&T> {
+//        //        let box  = rstar::object::RTreeObject
+////        let query_envelope = AABB::from_corners(
+////            envelope.ll.as_array(),
+////            envelope.ur.as_array(),
+////        );
+//
+//
+//        let elements = self.index.locate_in_envelope_intersecting(
+//            &envelope
+//        );
+//        let mut results = vec![];
+//        for o in elements.into_iter() {
+//            results.push(o)
+//        }
+//        results
+//    }
 
     pub fn nearest_neighbor(&self, query_pt: &<T::Envelope as Envelope>::Point) -> Option<&T> {
         self.index.nearest_neighbor(query_pt)
@@ -40,16 +71,16 @@ mod tests {
     #[test]
     fn it_works() {
         let items = vec![
-            MBR::new(pt!(480., 400.), pt!( 550., 484.)),
-            MBR::new(pt!(673., 336.), pt!( 773., 486.)),
-            MBR::new(pt!(285., 277.), pt!( 385.0, 377.0))
+            MBR::new(pt!(0., 0.), pt!( 1., 1.)),
+            MBR::new(pt!(1., 1.), pt!( 3., 3.)),
+            MBR::new(pt!(4., 2.), pt!( 7.0, 3.0))
         ];
         let mut tree = RTree::load(items);
 
-        let query = [526., 324.].into();
-        let res = tree.nearest_neighbor(&query);
-        if let Some(r) = res {
-            println!("intersecting {:?}", r);
-        }
+//        let query = MBR::new(pt!(1.5, 0.5), pt!( 4.0, 2.5));
+//        let res = tree.search(&query);
+//        if let Some(r) = res {
+//            println!("intersecting {:?}", r);
+//        }
     }
 }
